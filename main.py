d8eb2cb33e1214383c34c46bf79c26e2fa3028d9
headhunter225014 <105608219+headhunter225014@users.noreply.github.com>
2023-04-03 21:50:12 -0400

    Second commit: models setup

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class MyForm(QDialog):
         self.ui = loadUi('project_7_Damir_Zababuryn.ui', self)
         self.ui.pushButtonExit.clicked.connect(self.exitMethod)
 
-        #
+        #find a path to db
         db_file = 'veh_log.db'
         pathobj = Path(db_file)
         bool_is_there = pathobj.expanduser().is_file()
@@ -20,6 +20,7 @@ class MyForm(QDialog):
         if not bool_is_there:
             print("no path to db")
 
+        #create connection with db
         self.conn = QtSql.QSqlDatabase.addDatabase('QSQLITE')
         self.conn.setDatabaseName(db_file)
         self.conn.open()
@@ -33,13 +34,44 @@ class MyForm(QDialog):
         else:
             print("Connection has been established")
 
+        self.model = QtSql.QSqlRelationalTableModel()
+        self.delrow = -1
+
+        # Set up the model.
+        self.setupModels()
+
+        # Set up the mapper.
+        self.mapper = QDataWidgetMapper(self)
+        self.mapper.setModel(self.model)
+        self.mapper.setItemDelegate(QtSql.QSqlRelationalDelegate())
+
+        self.mapper.addMapping(self.ui.comboBox, self.project_index)  # related field in Projects table
 
 
 
 
     def exitMethod(self):
-        #self.conn.close()
+        self.conn.close()
         QApplication.instance().quit()
+
+    def setupModels(self):
+        self.model.setTable('Maintenance')  # for editable QSqlTableModel()
+        self.model.setEditStrategy(QtSql.QSqlTableModel.EditStrategy.OnFieldChange)  # for editable QSqlTableModel()
+
+        ############################################################################
+        # setup relationship of tasks.project_id to projects.id to get projects.name
+        self.project_index = self.model.fieldIndex("maint_vin")
+        self.model.setRelation(self.project_index,
+                               QtSql.QSqlRelation("Vehicles", "veh_vin", "veh_name"))
+        #############################################################################
+
+        self.model.select()  # used for editable QSqlTableModel()
+        #############################################################################
+        # setup relationship model and link it to the combobox
+        self.relModel = self.model.relationModel(self.project_index)
+        self.ui.comboBox.setModel(self.relModel)
+        self.ui.comboBox.setModelColumn(self.relModel.fieldIndex('veh_name'))
+        #############################################################################
 
 
 # the code below should not be changed and is constant for all GUI programs
